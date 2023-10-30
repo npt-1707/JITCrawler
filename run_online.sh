099c7694171b33d035b26194d1187e41539fb8e5
npt-1707 <npthanh132@gmail.com>
2023-10-30 20:36:48 +0700

    add bash

---
+++ run_online.sh
@@ -0,0 +1,10 @@
+python3 main.py \
+--save_path $1\
+--mode online\
+--ids_path $2\
+--num_commits_per_file 5000\
+--github_token_path $3\
+--github_owner $4\
+--github_repo $5\
+--to_csv True\
+--extract_features True\
