251fe0d0254ee5afd8d829b94bd17e7bdc20b164
npt-1707 <npthanh132@gmail.com>
2023-11-08 21:05:46 +0700

    add git_datasets folder

---
+++ run.py
@@ -0,0 +1,35 @@
+import os, argparse, subprocess
+from RepositoryExtractor import RepositoryExtractor
+def get_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--project", type=str, default="gerrit")
+    parser.add_argument("--prj", type=str, default="")
+    parser.add_argument("--mode", type=str, default="local")
+    parser.add_argument("--local_repo_path", type=str)
+    parser.add_argument("--ids_path", type=str, default="")
+    parser.add_argument("--num_commits_per_file", type=int, default=5000)
+    parser.add_argument("--save_path", type=str, default="save")
+    return parser.parse_args()
+
+params = get_params()
+
+os.chdir(f"git_datasets/{params.prj}")
+with open(f"{params.project}.txt", "r") as f:
+    clone_cmds = [cmd.strip("\n") for cmd in f.readlines()]
+for cmd in clone_cmds:
+    try:
+        os.system(cmd)
+    except:
+        IOError(f'Git Clone Error {cmd}')
+repos = [r for r in os.listdir(f"git_datasets/{params.prj}") if os.path.isdir(r)]
+assert len(repos) > 0, "No repos found"
+os.chdir("../../")
+
+extractor = RepositoryExtractor()
+
+for repo in repos:
+    params.prj = repo
+    params.local_repo_path = f"git_datasets/{params.project}/{params.prj}"
+    params.ids_path = f"git_datasets/{params.project}/{params.prj}.pkl"
+    params.save_path = f"save/{params.project}"
+    extractor.config_repo(params)
